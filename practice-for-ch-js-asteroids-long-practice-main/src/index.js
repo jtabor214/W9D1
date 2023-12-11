576640455b68051ff4a2955212b94ce5f6667c8f
Jacob Tabor <jtabor214@gmail.com>
2023-12-11 17:12:39 -0500

    Extended index.js, moving_object.js

--- practice-for-ch-js-asteroids-long-practice-main/src/index.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/index.js
@@ -1,9 +1,27 @@
+import MovingObject from "./moving_object.js";
+
+window.MovingObject = MovingObject;
+
 document.addEventListener('DOMContentLoaded', () => {
 
     console.log("Webpack is working!")
     const canvasEle = document.getElementById("game-canvas");
     canvasEle.width = 500;
     canvasEle.height = 500;
-    
+
+    const canvas_ctx = canvasEle.getContext('2d');
+    canvasEle.fillStyle = "black"
+
+    const test_sprite = new MovingObject({
+        pos: [30, 30],
+        vel: [10, 10],
+        radius: 5,
+        color: "#D21404"
+      });
+
+    test_sprite.draw(canvas_ctx)
+    test_sprite.move(76)
+    test_sprite.draw(canvas_ctx)
 
 })
+
---
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
@@ -0,0 +1,31 @@
+class MovingObject {
+
+    constructor(options){
+        this.pos = options.pos
+        this.vel = options.vel
+        this.radius = options.radius
+        this.color = options.color
+    }
+
+    draw(ctx){
+        ctx.fillStyle = this.color;
+
+        ctx.beginPath();
+        ctx.arc(
+            this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true);
+        ctx.fill();
+    }
+
+    move(timeDelta){
+        const velocityScale = timeDelta / FPS_TIME_DELTA, 
+        offsetX = this.vel[0] * velocityScale,
+        offsetY = this.vel[1] * velocityScale;
+
+        this.pos = [this.pos[0] + offsetX, this.pos[1] + offsetY];
+    }
+
+}
+
+const FPS_TIME_DELTA = 1000/60
+// module.exports = MovingObject;
+export default MovingObject
